886c5c27d9677d7b1f0b9701c2526b2818cca579
José Aricochea Rios <72884590+josearicochearios@users.noreply.github.com>
2021-11-15 14:20:47 -0500

    primer

---
+++ app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { YourGuardGuard } from './your-guard.guard';
+
+const routes: Routes = [{
+  path: 'first-component/:id', component: FirstComponent,
+  children: [
+    {path: 'child-a', component: ChildAComponent},
+    {path: 'child-b', component: ChildBComponent},
+  ]
+}, {
+  path: 'second-component', component: SecondComponent, canActivate: [YourGuardGuard]
+}, {
+  path: '**', component: FirstComponent
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
