3be8d77a15a288a1929fd2d01dceb0aa22908f49
José Aricochea Rios <72884590+josearicochearios@users.noreply.github.com>
2021-11-20 14:15:07 -0500

    second

--- src/app/greetings-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GreetingsPipePipe } from './greetings-pipe.pipe';
-
-describe('GreetingsPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new GreetingsPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
